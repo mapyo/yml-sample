b3af0397541cc79bfaa94ce6ada747804df488ce
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2015-01-06 09:17:23 +0900

    別パターンのyamlの読み込み方法も追加

--- test.php
+++ test.php
@@ -1,10 +1,14 @@
 <?php
+# ref. http://symfony.com/doc/current/components/yaml/introduction.html
 require 'vendor/autoload.php';
 
 use Symfony\Component\Yaml\Parser;
 
 $yaml = new Parser();
-
 $value = $yaml->parse(file_get_contents('test.yml'));
+var_dump($value);
 
+
+use Symfony\Component\Yaml\Yaml;
+$value = Yaml::parse(file_get_contents('test.yml'));
 var_dump($value);
